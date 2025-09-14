8964c97b9c8269c3c61e9cd99d10472008fe6f9d
ceejaymar <martinezcj2@gmail.com>
2025-09-14 19:00:41 -0400

    reorganize folders, add backlog & task pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 'use client';
 
-import { useTasks } from '@/app/store/tasks';
+import { useTasks } from '@/store/tasks';
+import { COLUMNS } from '../lib/mockData';
+import TaskColumn from './components/tasksColumn/task-column';
 
 export default function Home() {
   const tasks = useTasks((state) => state.tasks);
   const addTask = useTasks((state) => state.addTask);
 
   return (
-    <>
-      <div className="font-sans grid grid-rows-[20px_1fr_20px] min-h-screen">
-        {tasks.map((task) => {
-          return task.title;
+    <div className="flex justify-center h-full py-20 ">
+      <div className="font-sans grid grid-cols-3 gap-6 min-w-[1280px]">
+        {COLUMNS.map((column) => {
+          return <TaskColumn key={column.slug} column={column} tasks={tasks} />;
         })}
       </div>
-    </>
+    </div>
   );
 }
