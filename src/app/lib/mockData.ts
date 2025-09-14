84e65e00e88d614f2e275b770191894431af4031
ceejaymar <martinezcj2@gmail.com>
2025-09-14 14:59:07 -0400

    add lib folder with moackdata

---
+++ src/app/lib/mockData.ts
@@ -0,0 +1,48 @@
+const ASSIGNEES = [
+  { id: 1, name: 'Los', profileColor: 'blue' },
+  { id: 2, name: 'Mia', profileColor: 'red' },
+  { id: 3, name: 'Jim', profileColor: 'green' },
+  { id: 4, name: 'Zoe', profileColor: 'yellow' },
+  { id: 5, name: 'Max', profileColor: 'purple' }
+];
+
+const TASKS = [
+  {
+    id: 1,
+    title: 'Create mobile app wireframes',
+    description: 'Create a modern and responsive mobile app wireframes.',
+    status: 'SCHEDULED',
+
+    createdDate: '2025-09-14',
+    tags: ['design', 'mobile'],
+    assigneeID: 1
+  },
+  {
+    id: 2,
+    title: 'Create mobile app prototypes',
+    description: 'Create interactive prototypes for the mobile app.',
+    status: 'IN_PROGRESS',
+    createdDate: '2025-09-15',
+    tags: ['design', 'mobile', 'development'],
+    assigneeID: 1
+  },
+  {
+    id: 3,
+    title: 'Research Backend Solutions',
+    description:
+      'Investigate and compare backend technologies for the new project.',
+    status: 'COMPLETED',
+    createdDate: '2025-09-10',
+    tags: ['research'],
+    assigneeID: 3
+  },
+  {
+    id: 4,
+    title: 'Choose app color scheme',
+    description: 'Select the primary and secondary colors for the app',
+    status: 'SCHEDULED',
+    createdDate: '2025-09-16',
+    tags: ['design'],
+    assigneeID: 2
+  }
+];
