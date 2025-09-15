bfd1fbd4fef91cf80c067d80e61a51ed7d3e2f2a
ceejaymar <martinezcj2@gmail.com>
2025-09-15 16:30:08 -0400

    remove uuid, add nanoid

--- src/app/task/create/page.tsx
+++ src/app/task/create/page.tsx
@@ -1,32 +1,26 @@
 'use client';
 
 import { Toaster, toast } from 'sonner';
-import { v4 as uuid } from 'uuid';
 
 import { useTasks } from '@/store/tasks';
 import TaskForm from '@/app/components/taskForm/task-form';
+import { createNumericNumber } from '@/utils/createNumericNumber';
 
 export default function CreateTaskPage() {
   const addTask = useTasks((state) => state.addTask);
-
-  // const newTask = {
-  //   id: uuid(),
-  //   title: formData.get('title'),
-  //   description: formData.get('description'),
-  //   status: formData.get('status'),
-  //   createdDate: new Date().toISOString(),
-  //   tags: ['design', 'mobile'],
-  //   assigneeID: formData.get('assigneeID')
-  // };
 
   return (
     <div>
       <TaskForm
         onSubmit={(values, form) => {
           addTask({
-            id: uuid(),
             ...values,
-            createdDate: new Date().toISOString(),
+            id: createNumericNumber(),
+            createdDate: new Date().toLocaleDateString('en-US', {
+              year: 'numeric',
+              month: 'short',
+              day: 'numeric'
+            }),
             tags: []
           });
           toast.success(`Task added to ${values.status}`);
